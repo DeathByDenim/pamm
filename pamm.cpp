d7ce39af33f75943b3eeed3f07acb3ba12719384
DeathByDenim <jarno@jarno.ca>
2014-01-02 15:11:13 -0500

    Two minor bugs in the sorting of available mods.

--- pamm.cpp
+++ pamm.cpp
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define PAMM_VERSION "3.0.1"
+#define PAMM_VERSION "3.0.2"
 
 #include "pamm.h"
 #include "modmanager.h"
