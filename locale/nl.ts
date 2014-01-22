3fc0cef0c0f0691a5a814de3ff5580437f77c204
DeathByDenim <jarno@jarno.ca>
2014-01-22 12:41:02 -0500

    Dutch translation added.

---
+++ locale/nl.ts
@@ -0,0 +1,284 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl">
+<context>
+    <name>AvailableMod</name>
+    <message>
+        <location filename="availablemod.cpp" line="41"/>
+        <source>by</source>
+        <translation>door</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="48"/>
+        <location filename="availablemod.cpp" line="185"/>
+        <location filename="availablemod.cpp" line="306"/>
+        <source>INSTALLED</source>
+        <translation>GEÏNSTALLEERD</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="53"/>
+        <location filename="availablemod.cpp" line="311"/>
+        <source>UPDATE AVAILABLE</source>
+        <translation>UPDATE BESCHIKBAAR</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="58"/>
+        <location filename="availablemod.cpp" line="316"/>
+        <source>NOT INSTALLED</source>
+        <translation>NIET GEÏNSTALLEERD</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="65"/>
+        <source>REQUIRES</source>
+        <translation>BENODIGD</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="90"/>
+        <source>No description available</source>
+        <translation>Geen beschrijving beschikbaar</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="102"/>
+        <source>Install</source>
+        <translation>Installeren</translation>
+    </message>
+    <message>
+        <location filename="availablemod.cpp" line="248"/>
+        <source>Downloaded %1 times</source>
+        <translation>%1 keer gedownload</translation>
+    </message>
+</context>
+<context>
+    <name>InstalledMod</name>
+    <message>
+        <location filename="installedmod.cpp" line="51"/>
+        <source>by</source>
+        <translation>door</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="55"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="55"/>
+        <source>build</source>
+        <translation>build</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="69"/>
+        <source>REQUIRES</source>
+        <translation>BENODIGD</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="82"/>
+        <source>Update</source>
+        <translation>Updaten</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="86"/>
+        <source>Uninstall</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="327"/>
+        <source>depend on this mod.</source>
+        <translation>hangt van deze mod af.</translation>
+    </message>
+    <message>
+        <location filename="installedmod.cpp" line="328"/>
+        <source>Are you sure you want to uninstall?</source>
+        <translation>Weet u zeker dat u deze wilt verwijderen?</translation>
+    </message>
+</context>
+<context>
+    <name>ModManager</name>
+    <message>
+        <location filename="modmanager.cpp" line="645"/>
+        <source>Couldn&apos;t open ZIP file!</source>
+        <translation>Kon ZIP bestand niet openen!</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="646"/>
+        <source>Reason</source>
+        <translation>Reden</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="686"/>
+        <source>%1 depends on other mods.</source>
+        <translation>%1 hangt van andere mods af.</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="687"/>
+        <source>Do you want to install those?</source>
+        <translation>Wilt u die installeren?</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="753"/>
+        <source>Unmet requirements!</source>
+        <translation>Missende benodigdheden!</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="754"/>
+        <source>%1 is not installed, but is needed for this mod. Mod will not work.</source>
+        <translation>%1 is niet geïnstalleerd, maar is wel nodig voor deze mod. Mod zal niet werken.</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="870"/>
+        <source>Couldn&apos;t delete all of the files.</source>
+        <translation>Kon alle bestanden niet verwijderen.</translation>
+    </message>
+    <message>
+        <location filename="modmanager.cpp" line="871"/>
+        <source>See</source>
+        <translation>Zie</translation>
+    </message>
+</context>
+<context>
+    <name>PAMM</name>
+    <message>
+        <location filename="pamm.cpp" line="96"/>
+        <source>UI MOD MANAGER</source>
+        <translation>UI MOD BEHEERDER</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="135"/>
+        <source>NEWS</source>
+        <translation>NIEUWS</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="145"/>
+        <source>No mods found</source>
+        <translation>Geen mods gevonden</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="163"/>
+        <source>INSTALLED MODS</source>
+        <translation>GEÏNSTALLEERDE MODS</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="173"/>
+        <source>SHOW:</source>
+        <translation>TOON:</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="177"/>
+        <location filename="pamm.cpp" line="506"/>
+        <source>ALL</source>
+        <translation>ALLES</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="178"/>
+        <location filename="pamm.cpp" line="510"/>
+        <source>INSTALLED</source>
+        <translation>GEÏNSTALLEERD</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="179"/>
+        <location filename="pamm.cpp" line="514"/>
+        <location filename="pamm.cpp" line="609"/>
+        <source>REQUIRE UPDATE</source>
+        <translation>UPDATE NODIG</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="180"/>
+        <location filename="pamm.cpp" line="518"/>
+        <source>NOT INSTALLED</source>
+        <translation>NIET GEÏNSTALLEERD</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="192"/>
+        <location filename="pamm.cpp" line="262"/>
+        <location filename="pamm.cpp" line="530"/>
+        <source>RANDOM</source>
+        <translation>WILLEKEURIG</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="193"/>
+        <location filename="pamm.cpp" line="534"/>
+        <source>LAST UPDATED</source>
+        <translation>LAATST GEÜPDATE</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="194"/>
+        <location filename="pamm.cpp" line="538"/>
+        <source>TITLE</source>
+        <translation>TITEL</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="195"/>
+        <location filename="pamm.cpp" line="542"/>
+        <source>AUTHOR</source>
+        <translation>AUTEUR</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="196"/>
+        <location filename="pamm.cpp" line="546"/>
+        <source>BUILD</source>
+        <translation>BUILD</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="197"/>
+        <location filename="pamm.cpp" line="550"/>
+        <source>DOWNLOADS</source>
+        <translation>DOWNLOADS</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="198"/>
+        <location filename="pamm.cpp" line="554"/>
+        <source>LIKES</source>
+        <translation>LEUK GEVONDEN</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="214"/>
+        <source>AVAILABLE MODS</source>
+        <translation>BESCHIKBARE MODS</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="219"/>
+        <location filename="pamm.cpp" line="390"/>
+        <source>Update all mods</source>
+        <translation>Update alle mods</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="229"/>
+        <source>Launch PA</source>
+        <translation>Start PA</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="233"/>
+        <source>Refresh</source>
+        <translation>Vernieuwen</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="237"/>
+        <source>Exit</source>
+        <translation>Afsluiten</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="241"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="245"/>
+        <source>, created by DeathByDenim (based on Raevn)</source>
+        <translation>, gemaakt door DeathByDenim (gebaseerd op Raevn)</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="462"/>
+        <source>Couldn&apos;t write to</source>
+        <translation>Kon niet schrijven naar</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="586"/>
+        <source>New update available for the PA Mod Manager.</source>
+        <translation>Nieuwe update beschikbaar voor de PA Mod Beheerder</translation>
+    </message>
+    <message>
+        <location filename="pamm.cpp" line="587"/>
+        <source>Download now?</source>
+        <translation>Nu downloaden?</translation>
+    </message>
+</context>
+</TS>
